c4eb21a9bfbcc73297149836f2a1f42f9a86d850
lyhcode <lyhcode@gmail.com>
2015-08-13 18:31:28 +0800

    add sails-service-mailer

---
+++ api/controllers/MailController.js
@@ -0,0 +1,25 @@
+module.exports = require('sails-service-mailer');
+
+var ses = MailerService.create('ses', {
+  from: 'no-reply@ghaiklor.com',
+  transporter: {
+    ses: {}, // instantiated AWS SES object with new AWS.SES()
+    accessKeyId: 'MY_KEY', // AWS access key
+    secretAccessKey: 'MY_SECRET', // AWS secret key
+    sessionToken: '', // Session token
+    region: '', // Specify the region to send the service request
+    httpOptions: {}, // A hash of options to pass to the low-level AWS HTTP request
+    rateLimit: 5 // Specify the amount of messages can be sent in 1 second
+  }
+});
+
+module.exports = {
+  send: function(req, res) {
+    ses
+      .send({
+        to: req.param('to')
+      })
+      .then(res.ok)
+      .catch(res.serverError);
+  }
+};
