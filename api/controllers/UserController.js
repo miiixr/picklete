3d499c698db7af17d6393fe21844e38d50b1bad2
dmoon <dmoon.t@gmail.com>
2015-08-25 00:22:03 +0800

    include jquery , bootstrap.js , bootstrap fonts 後台頁面view與route架構完成 畫面有少數地方間隔不對，待修bug

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -32,7 +32,124 @@ UserController = {
     res.view({
       errors: req.flash('error')
     });
+  },
+  controlBrands: function (req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlBrandsDetail: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlShopType: function (req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlShopItemAdd: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlShopItemList: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlShopDiscount: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlShopDiscountDetail: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlShopDiscountDetail2: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlShopDiscountAddItem: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlShopBuyMore: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlShopBuyMoreDetail: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlShopBuyMoreAddItem: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlShopCode: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlShopCodeDetail: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlShopReportForm: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlOrder: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlAbout: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlQa: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlQaDetail: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlQaType: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlQaAdd: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlMembers: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
+  },
+  controlMemberDetail: function(req, res) {
+    res.view({
+      errors: req.flash('error')
+    });
   }
+
+
 };
 
 module.exports = UserController;
