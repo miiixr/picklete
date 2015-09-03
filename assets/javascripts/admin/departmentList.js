af59c6139c339438207c12f556fa957fd00648a4
clonn <clonncd@gmail.com>
2015-09-04 01:56:19 +0800

    fix js style and update css

--- assets/javascripts/admin/departmentList.js
+++ assets/javascripts/admin/departmentList.js
@@ -1,48 +1,45 @@
- $(window).load(function(){
-
+(function ($) {
 
 
     $('.glyphicon.glyphicon-pencil2.m-left-1').click(function(event){
         var e = $(event.currentTarget);
-        document.getElementById('lg-id').value=e.data('id');
-        document.getElementById('lg').value=e.data('name');
+        document.getElementById('lg-id').value = e.data('id');
+        document.getElementById('lg').value = e.data('name');
         $('#modal-control-shop-type-lg').modal('show'); 
-    })
-
-
+    });
 
     $('.dpt.name').click(function(event){
         var e = $(event.currentTarget);
-        var id = e.data('id')-1;
-        for(i=0;i<=5;i++){
-            document.getElementById('subDpt'+i).style.display="none";
+        var id = e.data('id') - 1;
+        for(i=0; i<=5; i++) {
+            document.getElementById('subDpt' + i).style.display = "none";
         }
-        document.getElementById('special').style.display="none";
-        document.getElementById('subDpt'+id).style.display="";
+        document.getElementById('special').style.display = "none";
+        document.getElementById('subDpt'+id).style.display = "";
         document.getElementById('sm-id').value = e.data('id');
         $('.btn.btn-sm-circle.btn-green.m-left-1').css("display","");
-    })
+    });
 
 
     $('.special.project').click(function(event){
-        for(i=0;i<=5;i++){
-            document.getElementById('subDpt'+i).style.display="none";
+        for(i=0; i<=5; i++){
+            document.getElementById('subDpt'+i).style.display = "none";
         }
-        document.getElementById('special').style.display="";
+        document.getElementById('special').style.display = "";
         $('.btn.btn-sm-circle.btn-green.m-left-1').css("display","none");
-    })
+    });
 
 
     $('.subDpt.name').click(function(event){
         var e = $(event.currentTarget);
-        document.getElementById('set-sm-id').value=e.data('id');
-        document.getElementById('set-sm').value=e.data('name');
+        document.getElementById('set-sm-id').value = e.data('id');
+        document.getElementById('set-sm').value = e.data('name');
         $('#modal-control-shop-type-sm-update').modal('show'); 
-    })
+    });
 
     $('.glyphicon.glyphicon-remove').click(function(event){
         var e = $(event.currentTarget);
-        document.getElementById('del-sm-id').value=e.data('id');
+        document.getElementById('del-sm-id').value = e.data('id');
         $('#modal-delete').modal('show'); 
-    })
-  })
+    });
+}(jQuery));
