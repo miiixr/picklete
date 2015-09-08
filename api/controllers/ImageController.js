c79aec5120ad39ad71eb88b576b58aecc508d3f1
clonn <clonncd@gmail.com>
2015-09-08 13:13:32 +0800

    update  image controler and process

--- api/controllers/ImageController.js
+++ api/controllers/ImageController.js
@@ -1,6 +1,9 @@
+let domain = sails.config.domain || process.env.domain || 'http://localhost:1337';
+
 module.exports = {
   upload: async (req, res) => {
     req.file("uploadfile").upload((err, files) => {
+      console.log(files)
 
       if ( ! files.length)
         return res.ok([]);
