ea6ef1c006bf40cdb03c7079f17356b0705d48bb
smlsunxie <smlsun@gmail.com>
2015-09-09 17:25:10 +0800

    add get SelectionActive spec

--- test/unit/services/SelectionActiveService.spec.js
+++ test/unit/services/SelectionActiveService.spec.js
@@ -1,4 +1,33 @@
 describe("SelectionActiveService", () => {
+  it('get SelectionActive', async (done) => {
+
+    // 測試資料從
+    try {
+
+      let result = await SelectionActiveService.getModel();
+
+      let selectionActives = result;
+
+      selectionActives.length.length.should.be.equal(4);
+      selectionActives[0].type.should.be.equal("oneLong");
+      selectionActives[0].Images.length.should.be.equal(1);
+
+      selectionActives[1].type.should.be.equal("oneBig");
+      selectionActives[1].Images.length.should.be.equal(1);
+
+      selectionActives[2].type.should.be.equal("two");
+      selectionActives[2].Images.length.should.be.equal(2);
+
+      selectionActives[3].type.should.be.equal("three");
+      selectionActives[3].Images.length.should.be.equal(3);
+
+
+
+    } catch (e) {
+      done(e);
+    }
+  });
+
   it('save SelectionActive', async (done) => {
 
     // 每次更新皆刪除重建 SelectionActive
