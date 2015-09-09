2411c045d75971f74832ad4d109149e1930c0ead
dmoon <dmoon.t@gmail.com>
2015-09-09 20:45:56 +0800

    add imageMultipartUpload and include in _auth_scripts.jade

---
+++ assets/javascripts/admin/imageMultipartUpload.js
@@ -0,0 +1,28 @@
+(function ($) {
+  var inputName;
+  $("input.fileInput").on('change.bs.fileinput', function (e) {
+    // console.log(e);
+    // console.log("click from select");
+    inputName = $(this).data('name');
+    var $form = $(this).parents("form").first();
+    $form.ajaxSubmit(options);
+  });
+
+  var options = {
+    beforeSubmit:  function (formData, jqForm, options) {
+      console.log(formData);
+      // var queryString = $.param(formData);
+    },
+    success: function (responseText, statusText, xhr, $form)  {
+      console.log(responseText);
+      console.log(statusText);
+      if(statusText == 'success') {
+        $('input[name='+inputName+']').val(responseText);
+      }
+    }
+  };
+
+  // $('form#brand-avatar').submit(function() {
+  //   return false;
+  // });
+}(jQuery));
