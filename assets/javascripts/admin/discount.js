027019d43d6b3db953a3c181f88baa93844f9b53
kuyen <iamcxa@gmail.com>
2015-09-09 20:13:32 +0800

    pre-built ajax function for click add a 閃購 button

--- assets/javascripts/admin/discount.js
+++ assets/javascripts/admin/discount.js
@@ -1,5 +1,15 @@
 $(window).load(function() {
 
-
+  // click add a 閃購 button
+  $(".btn.btn-green.btn-lg").click(function() {
+    var className = $(this).find( "label:first" ).attr('class');
+    var id = $(this).attr('id');
+    var path = "/product/unpublish/" + id;
+    $.ajax({
+      type: "PUT",
+      url: path
+    });
+  });
+  // end click add a 閃購 button
 
 });
