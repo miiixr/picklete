8fff5b6ce20e9be29ca90e94ebbc7be478a39f53
smlsunxie <smlsun@gmail.com>
2015-09-16 13:59:37 +0800

    fix shop list display.

--- api/controllers/ShopController.js
+++ api/controllers/ShopController.js
@@ -2,39 +2,61 @@
 let ShopController = {
 
   list: async(req,res) => {
-    let dptSubId = req.query.dptSubId || "error";
-    let dptId = req.query.dptId || "error"; 
+    let dptSubId = req.query.dptSubId || -1;
+    let dptId = req.query.dptId || -1;
 
     let products;
 
-    products = await db.Product.findAll({
 
-      include: [{
-        model: db.ProductGm,
-        require:true,
-        where: {
-          dptId: dptId,
-          dptSubId: dptSubId
-        }
-      }]
-    });
+    let includeDpt = {
+      model: db.Dpt,
+      where: {}
+    }
 
-    return db.Dpt.findAll({
-      include: [{
-        model: db.DptSub
-      }],
-      order: ['Dpt.weight', 'DptSubs.weight']
-    })
-    .then(function(dpts) {
+    let includeDptSub = {
+      model: db.DptSub,
+      where: {}
+    }
+    if(dptId >= 0) includeDpt.where.id = dptId;
+    if(dptSubId >= 0) includeDptSub.where.id = dptSubId;
+
+
+
+
+    try {
+      products = await db.Product.findAll({
+
+        include: [{
+          model: db.ProductGm,
+          require:true,
+          include: [
+            includeDpt,
+            includeDptSub
+          ]
+        }]
+      });
+
+      console.log('products.length', products.length);
+
+      let dpts = await db.Dpt.findAll({
+        include: [{
+          model: db.DptSub
+        }],
+        order: ['Dpt.weight', 'DptSubs.weight']
+      })
+
+
       res.view('main/shop', {
         dpts,
         products: products || {},
         pageName: 'main/shop'
       });
-    })
-    .catch(function(error) {
-      return res.serverError(error);
-    });
+
+
+    } catch (e) {
+
+      return res.serverError(e);
+    }
   }
 }
 
