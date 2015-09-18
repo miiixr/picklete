be5cb3200152ca8499b0be1daded7869ca347dac
dmoon <dmoon.t@gmail.com>
2015-09-18 17:29:39 +0800

    update themeActivities.js : multipart upload image then write back to hidden input

--- assets/javascripts/admin/themeActivities.js
+++ assets/javascripts/admin/themeActivities.js
@@ -11,47 +11,69 @@ $(function  () {
       }
     });
   });
-  
+
   var $weightCount = 1;
   var $imageSection = $('li.imageSection')[0].outerHTML;
+
+  // image container + icon click event
   $('body').on('click','.btn-add',function(e){
     $('ul.imageSection').append($imageSection);
     $('li.imageSection').last().find('input').attr('value', $weightCount);
     $weightCount = $weightCount + 1;
-    
+
   });
-  
-  var $weight, $id;
+  var $uploadFileName = $('#uploadFileName'),
+      $openWindow = $('input#openWindow'),
+      $productUrl1 = $('#productUrl1'),
+      $productUrl2 = $('#productUrl2'),
+      $fileInputPath = $('#fileInputPath');
+
+  var weight, imageContainerId, $imageContainer;
   $(document).on("click", ".fileinput-square a", function (e) {
-    var that = $(this);
-    $weight = parseInt(that.parent().parent().parent().parent().parent().parent().parent().find('input[name="weight"]').val());
-    $id = that.data('id');
-    console.log($weight, $id);
-
+    $imageContainer = $(this);
+    // $weight = parseInt(that.parent().parent().parent().parent().parent().parent().parent().find('input[name="weight"]').val());
+    imageContainerId = $imageContainer.data('id');
   });
 
-  $('.modal-content').on('click', '.btn-green', function(e){
+
+  // modal confirm button - click event
+  $('.modal-content').on('click', '.btn-green', function(e) {
     e.preventDefault();
-    var that = $(this);
-    var $url = '';
-    $ratio = $('input[name=optionsRadios]:checked').val(); 
-    $checkbox = $('input[type=checkbox]:checked').val() || false; 
-    $productUrl1 = $('#productUrl1').val();
-    $productUrl2 = $('#productUrl2').val();
-    $fileinput_url = '<img src="http://fakeimg.pl/600x600/dddddd/FFF/?text=600x600" class="img-full">';
-    $target = $("input[value='"+$weight+"']").parent().parent().parent().find("a[data-id='"+$id+"']");
-    
-    if($ratio == 'option1'){
-      $url = $productUrl1;
+
+    var that = $(this),
+        url = '',
+        path = '',
+        $openWindow = $('#openWindow');
+        $modalRatio = $('input[name=optionsRadios]:checked'),
+        modalRatioValue = $modalRatio.val(),
+        productUrl1Value = $productUrl1.val(),
+        productUrl2Value = $productUrl2.val(),
+        fileInputPath = $fileInputPath.val();
+
+    // $fileinput_url = '<img src="http://fakeimg.pl/600x600/dddddd/FFF/?text=600x600" class="img-full">';
+    // $target = $("input[value='"+weight+"']").parent().parent().parent().find("a[data-id='"+imageContainerId+"']");
+
+    if(modalRatioValue == 'option1'){
+      // path = productUrl1Value;
+      // url = productUrl1Value;
     }
     else{
-      $url = $productUrl2;
-      $target.append($fileinput_url);
+      path = fileInputPath;
+      url = productUrl2Value;
+      // $target.append($fileinput_url);
     }
 
-    $target.attr('href', $url);
-    
-  });
+    // add image openWindow, path & url back to form hidden input
+    $imageContainer.find('input[data-content="path"]').val(path);
+    $imageContainer.find('input[data-content="url"]').val(url);
+    if($openWindow.is(':checked'))
+      $imageContainer.find('input[data-content="openWindow"]').val('true');
 
+    // add image
+    $imageContainer.find('img').attr('src',path);
+
+    // $target.attr('href', url);
+
+  });
 
 });
