d53150465047b395921ef6959a5a9150ab09bb0a
kevin steve <qqwwee2006@gmail.com>
2015-09-18 23:09:28 +0800

    model test finsih

--- api/controllers/FAQController.js
+++ api/controllers/FAQController.js
@@ -1,22 +1,35 @@
 let FAQController = {
   FAQ : async(req,res) => {
-    let FAQTypes = await db.FAQType.findAll({
-      include: [{
-        model: db.FAQ
-      }]
-    })
-    return res.view("user/controlFAQ", {
-      FAQTypes: FAQTypes
-    });
+    try{
+
+      let FAQTypes = await db.FAQType.findAll({
+        include: [{
+          model: db.FAQ
+        }]
+      })
+
+      return res.view("user/controlFAQ", {
+        FAQTypes: FAQTypes
+      });
+
+    } catch(e){
+      console.error(e.stack);
+      let msg = e.message;
+      return res.serverError({msg});
+    }
   },
+
   FAQAdd : async(req,res) => {
     try{
+
       let FAQTypes = await db.FAQType.findAll();
+
       if(req.method == "GET"){
         return res.view("user/controlFAQAdd",{
          FAQTypes: FAQTypes || null
         });
       }
+
       var FAQData = {
         title: req.body.title,
         answer: req.body.answer,
@@ -35,27 +48,33 @@ let FAQController = {
       console.log(e);
     }
   },
+
   FAQUpdate : async(req,res) => {
+   
     try{
+     
       var FAQId = req.query.FAQId;
       let FAQ = await db.FAQ.findById(FAQId);
       let FAQTypes = await db.FAQType.findAll();
+      
       if(req.method == "GET"){
         return res.view("user/controlFAQAdd",{
           FAQ :FAQ,
           FAQTypes : FAQTypes || null
         });
       }
+      
       var params = req.body;
 
       if (! params) {
         return res.redirect("/admin/FAQ");
+      
       }
       FAQ.title = params["title"];
       FAQ.FAQTypeId = params["typeId"];
       FAQ.answer = params["answer"];
-      console.log(FAQ);
       let updatedFAQ = await FAQ.save();
+      
       return res.redirect("/admin/FAQ");
 
     } catch(e){
@@ -63,6 +82,7 @@ let FAQController = {
       return res.redirect("/admin/FAQ");
     }
   },
+
   FAQTypeUpdate : async(req,res) => {
     try{
       let FAQTypes = await db.FAQType.findAll();
@@ -72,6 +92,7 @@ let FAQController = {
         });
       }
       var name = req.body.name;
+      
       for(var i=0;i<name.length;i++){
         if(name[i] != ""){
           var createTypeData = await db.FAQType.create({
@@ -79,9 +100,11 @@ let FAQController = {
           });
         }
       }
+      
       try{
         var FAQTypeId = req.body.FAQTypeId;
         var FAQType = req.body.FAQType;
+
         for(var i=0;i<FAQTypeId.length;i++){
           var updateFAQType = await db.FAQType.findById(FAQTypeId[i]);
           if(FAQType[i]!=""){
@@ -89,27 +112,34 @@ let FAQController = {
             var update = updateFAQType.save();
           }
         }
+        
       } catch(e){
         console.log(e);
       }
+      
       return res.redirect("/admin/FAQ");
     } catch(e){
       console.log(e);
     }
   },
+
   FAQDelete : async(req,res) => {
+    
     try{
       var FAQDelete = await db.FAQ.destroy({
         where: {
           id :req.body.id
         }
       });
+      
       return res.redirect("/admin/FAQ");
     } catch(e){
       console.log(e);
     }
   },
+
   FAQTypeDelete : async(req,res) => {
+    
     try{
       var FAQDelete = await db.FAQType.destroy({
         where: {
@@ -117,6 +147,7 @@ let FAQController = {
         }
       });
       return res.redirect("/admin/FAQTypeUpdate");
+    
     } catch(e){
       console.log(e);
     }
