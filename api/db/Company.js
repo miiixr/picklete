4afeb026e03a04f86a9e641f01a78bf28865fee7
clonn <clonncd@gmail.com>
2015-09-19 16:24:48 +0800

    add db comapny

---
+++ api/db/Company.js
@@ -0,0 +1,26 @@
+
+module.exports = function(sequelize, DataTypes) {
+
+  var Company = sequelize.define('Company', {
+    // 公司圖片位置
+    avatar: DataTypes.STRING,
+    
+    // 公司名稱
+    name: DataTypes.STRING,
+
+    // 公司完整名稱
+    fullname: DataTypes.STRING,
+
+    // 公司聯絡 email
+    email: DataTypes.STRING,
+
+    // 公司介紹
+    desc: DataTypes.TEXT,
+
+    // 公司的 line image
+    line: DataTypes.STRING
+
+  });
+
+  return Company;
+};
