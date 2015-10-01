24f307d06072d24baec603bfb633c86b03282096
kuyen <iamcxa@gmail.com>
2015-10-01 20:21:19 +0800

    implement pricing.

--- api/services/PromotionService.js
+++ api/services/PromotionService.js
@@ -20,15 +20,24 @@ module.exports = {
   create: async (promotion) => {
     try {
       console.log('=== raw promotion ==>',promotion);
+
       if(promotion.discount =='')
         delete promotion.discount;
       if(promotion.price=='')
         delete promotion.price;
+
       let createdPromotion = await db.Promotion.create(promotion);
       console.log('=== createdPromotion ==>',createdPromotion);
+
+      let promotedProducts = await* promotion.productGmIds.map(async (productGmId)=>{
+        let findProductGm = await db.ProductGm.findById(productGmId);
+        await createdPromotion.setProductGms([findProductGm]);
+        return createdPromotion;
+      });
+
       return createdPromotion;
     } catch (e) {
-      console.log('=== create err ==>',e);
+      console.log('=== create err ==>',e.stack);
       return false;
     }
   },
@@ -53,7 +62,7 @@ module.exports = {
       await updatePromoiton.save();
       return updatePromoiton;
     } catch (e) {
-      console.log('=== update err ==>',e);
+      console.log('=== update err ==>',e.stack);
       return false;
     }
   },
@@ -63,14 +72,14 @@ module.exports = {
   delete: async (promotion) => {
     try {
       console.log('=== raw promotion ==>',promotion);
-        let findProduct = await db.Product.findById(promotion.id);
+      let findProduct = await db.Product.findById(promotion.id);
       if (!findPromotion) {
         throw new Error('cant find this promotion! id is ==>',promotion.id);
       }
       await findPromotion.destroy();
       return true;
     } catch (e) {
-      console.log('=== delete err ==>',e);
+      console.log('=== delete err ==>',e.stack);
       return false;
     }
   },
@@ -78,11 +87,67 @@ module.exports = {
 
   // pricing
   pricing: async(promotion) => {
-    // let promotion;
     try {
-      console.log('=== promotion is ==>',promotion);
+      // console.log('=== raw promotion is ==>',promotion);
+      var now = new Date();
+
+      // find promotion and productGms,products
+      let findPromotionProductGms = await db.Promotion.findAll({
+        where:{
+          startDate: {
+            lt: now
+          },
+          endDate: {
+            gte: now
+          }
+        },
+        include: [{
+          model: db.ProductGm,
+          include: [db.Product]
+        }]
+      });
+
+      // set price from promotion settings
+      await* findPromotionProductGms.map(async (findPromotionProductGm) => {
+        // console.log('=== findPromotionProductGm.discountType ==>',findPromotionProductGm.discountType);
+        // console.log('=== findPromotionProductGm ==>',findPromotionProductGm);
+        // console.log('=== findPromotionProductGm.ProductGms ==>',findPromotionProductGm.ProductGms);
+        let productGms = findPromotionProductGm.ProductGms;
+        let discountType = findPromotionProductGm.discountType;
+
+        if(productGms.length != 0){
+          // check productGms for each
+          await* productGms.map(async (productGm) => {
+            // console.log('=== productGm.id ==>',productGm.id);
+            // console.log('=== productGm.Products.length ==>',productGm.Products.length);
+            // get origin price
+            productGm.originPrice = productGm.Products[0].price;
+
+            if(productGm.Products.length != 0){
+              // set new price from promotion for each one
+              await* productGm.Products.map(async (product) => {
+                // console.log('=== product.id ==>',product.id);
+                // depends on discountType
+                if(discountType == 'discount'){
+                  // console.log('=== findPromotionProductGm.discount ==>',findPromotionProductGm.discount);
+                  product.price = product.price * findPromotionProductGm.discount;
+                }else if(discountType == 'price'){
+                  // console.log('=== findPromotionProductGm.price ==>',findPromotionProductGm.price);
+                  product.price = findPromotionProductGm.price;
+                }
+                return await product.save();
+              });
+            }
+            return await productGm.save();
+          });
+        }
+        return findPromotionProductGm;
+      });
+
+      return findPromotionProductGms;
     } catch (e) {
-      console.log('=== delete err ==>',e);
+      console.log('=== pricing err ==>',e.stack);
+      throw e;
       return false;
     }
   }
