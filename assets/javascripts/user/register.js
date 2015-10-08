6dd8f7e1b5e2b599a7a1d17ccbf72687b591bdea
fuyaode <dan82625@gmail.com>
2015-10-08 15:12:39 +0800

    add register fail

--- assets/javascripts/user/register.js
+++ assets/javascripts/user/register.js
@@ -11,7 +11,7 @@ $(window).load(function() {
       'form-control width-auto inline-block',
       'form-control width-auto inline-block']
   });
-  
+
   $("#registerForm").on('submit', function(e) {
     e.preventDefault();
     if($("#password").val() != $("#passwordCheck").val()){
@@ -21,8 +21,31 @@ $(window).load(function() {
       return alert('密碼太短');
     }
     else{
-      $(this).off('submit');
-      $(this).submit();
+      // $(this).off('submit');
+      // $(this).submit();
+      $.ajax({
+          type: 'post',
+          url: $(this).attr('action'),
+          data: $(this).serialize(),
+          dataTypes: 'html',
+          success:function(data, textStatus, jqXHR){
+            console.log(data);
+            if (data.status == "ok") {
+              alert("記得去收驗證信喔");
+              window.location.replace("/shop/products");
+            }else{
+              $(this).notifyMe(
+                'top', // Position
+                'error', // Type
+                'Lorem Ipsum Text', // Title
+                'Lorem Ipsum is simply dummy text of the printing', // Description
+                200, // Velocity of notification
+                2000 // (optional) Time of delay to close automatically
+              );
+            }
+          }
+      })
+      return false;
     }
   });
 
