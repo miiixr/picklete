eb0efb6eeebecd089795eccbb00bfa72e51f0ad6
lyhcode <lyhcode@gmail.com>
2015-10-14 12:10:35 +0800

    move mail action from main to message controller

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -1,8 +1,4 @@
-import {sprintf} from 'sprintf-js';
-import moment from 'moment';
-
 module.exports = {
-
   index: async (req, res) => {
     try {
       let selectionActivities = await SelectionActiveService.getModel();
@@ -20,41 +16,5 @@ module.exports = {
       let {message} = e;
       res.serverError({message, success: false});
     }
-  },
-
-  mail: async (req, res) => {
-    try {
-      let user = {
-        username: 'test',
-        email: 'lyhcode@gmail.com'
-      };
-      var greetingTpl = sails.config.mail.templete.greeting;
-      var email = user.email;
-      var mailSendConfig = {...greetingTpl, from: sails.config.mail.config.from, to: email};
-
-      mailSendConfig.subject = sprintf(mailSendConfig.subject, {
-        username: user.username
-      });
-
-      mailSendConfig.html = sprintf(mailSendConfig.html, {
-        username: user.username,
-        storeName: sails.config.store.name,
-        storeName2: sails.config.store.name2,
-        storeName3:sails.config.store.name3,
-        serviceMail: sails.config.store.serviceMail,
-      });
-
-      mailSendConfig.type = 'greeting';
-
-      let send = await sails.config.mail.mailer.send(mailSendConfig);
-
-      res.ok(send);
-
-      //res.ok(mailSendConfig);
-    } catch (e) {
-      console.error(e);
-      throw error;
-    }
   }
-
 }
