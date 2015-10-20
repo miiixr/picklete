08ddc95544b3e0a9396b78434860755f26e2e857
clonn <clonncd@gmail.com>
2015-10-20 10:11:37 +0800

    hotfix exclusive add fucntion

--- assets/javascripts/admin/exclusive.js
+++ assets/javascripts/admin/exclusive.js
@@ -12,28 +12,40 @@
     });
   });
 
-  var $type1 = $('.type1')[0].outerHTML;
-  var $type2 = $('.type2')[0].outerHTML;
-  var $type3 = $('.type3')[0].outerHTML;
-  var $type4 = $('.type4')[0].outerHTML;
+  // var $type1 = $('.type1')[0].outerHTML;
+  // var $type2 = $('.type2')[0].outerHTML;
+  // var $type3 = $('.type3')[0].outerHTML;
+  // var $type4 = $('.type4')[0].outerHTML;
+
+
+  var $type1 = $('#type1')[0].innerHTML;
+  var $type2 = $('#type2')[0].innerHTML;
+  var $type3 = $('#type3')[0].innerHTML;
+  var $type4 = $('#type4')[0].innerHTML;
+
 
   $('#modal-control-index-exclusive-type').on('click','.btn-green',function(e){
     e.preventDefault();
     var $checked = $('input:checkbox:checked[name="exclusive-type"]').map(function() { return $(this).val(); }).get();
 
     for (var i in $checked){
+
+
+      var itemLength = $('.well');
+      var index = itemLength ? itemLength.length : 0;
+
       var check = $checked[i];
       if(check == 1){
-        $('ul.col-sm-9.col-md-10').append($type1);
+        $('.col-sm-9.col-md-10').append($type1.replace(/{index}/g, index));
       }
       if(check == 2){
-        $("ul.col-sm-9.col-md-10").append($type2);
+        $(".col-sm-9.col-md-10").append($type2.replace(/{index}/g, index));
       }
       if(check == 3){
-        $("ul.col-sm-9.col-md-10").append($type3);
+        $(".col-sm-9.col-md-10").append($type3.replace(/{index}/g, index));
       }
       if(check == 4){
-        $("ul.col-sm-9.col-md-10").append($type4);
+        $(".col-sm-9.col-md-10").append($type4.replace(/{index}/g, index));
       }
     }
 
@@ -49,6 +61,12 @@
       console.log( key + ": " + dom.value);
       dom.value = "" ;
     });
+
+
+
+    
+
+
 
   });
 
