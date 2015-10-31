0879b3df869c159b28905052e0a32d08865b31c8
clonn <clonncd@gmail.com>
2015-10-31 08:52:38 +0800

    count down timer

--- assets/javascripts/main/index.js
+++ assets/javascripts/main/index.js
@@ -1,30 +1,41 @@
 (function ($) {
-  console.log('run');
+  
+  var runTimer = function (differ, target) {
+    var durationTime = differ.asMilliseconds();
+    setInterval(function(){
+
+      var duration = moment.duration(durationTime, 'milliseconds');
+      durationTime -= interval;
+      //show how many hours, minutes and seconds are left
+      target.find('span').text(moment(duration.asMilliseconds()).format('h:mm:ss'));
+    }, interval);
+
+  };
+
   var promos = $('.promo-end');
   if (promos.length < 1)
     return;
 
   var interval = 1000;
   $.each(promos, function (idx, promo) {
+    
     var self = $(promo);
     var end = self.data('end');
+    
     if ( ! end)
-      return self.find('span').text('不限時');  
+      return self.find('span').text('不限時'); 
+
     var eventTime = moment(end).valueOf();
     var currentTime = moment().valueOf(); // Timestamp - Sun, 21 Apr 2013 12:30:00 GMT
     var diffTime = eventTime - currentTime;
-    var duration = moment.duration(diffTime*1000, 'milliseconds');
-    duration = moment.duration(duration - interval, 'milliseconds');
-    self.find('span').text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds());
 
-    // var eventTime = moment().valueOf();
-    // var currentTime = moment().valueOf();
-    // var diffTime = eventTime - currentTime;
-    // var duration = moment.duration(diffTime*1000, 'milliseconds');
-    
-    // duration = moment.duration(duration - interval, 'milliseconds');
-    
+    var duration = moment.duration(diffTime, 'milliseconds');
+
+    runTimer(duration, self);    
   });
+
+
+
 
 
 
